ad6e8fb275ce9ac44c3306da09e43395700949c4
Atharav Sharma <atharav842@gmail.com>
2025-09-24 19:41:00 +0530

    Create S2C09.cpp

---
+++ Set-2/S2C09.cpp
@@ -0,0 +1,38 @@
+
+//Pointer to Array of Pointers 
+/*Create an array of string pointers (e.g., names of 5 students) and print them using  
+pointer notation */
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    const char* students[5] = {
+        "Abc",
+        "def", 
+        "ghi",
+        "jkl",
+        "mno"
+    };
+    
+    cout << "Student names using pointer notation:" << endl;
+    
+    for(int i = 0; i < 5; i++) {
+        cout << "Student " << i+1 << ": " << *(students + i) << endl;
+    }
+    
+    cout << "\nAlternative pointer notation:" << endl;
+    
+    const char** ptr = students;
+    for(int i = 0; i < 5; i++) {
+        cout << "Student " << i+1 << ": " << *ptr << endl;
+        ptr++; pointer
+    }
+    
+    return 0;
+}
